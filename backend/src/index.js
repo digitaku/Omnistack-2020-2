fd213855055b606305a78a78023ec9e8633e050a
digitaku <atilasabc@gmail.com>
2020-03-25 00:05:34 -0300

    backend completo.

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+app.use(cors());
+
+//usando para que se possa compreender as request como json
+app.use(express.json());
+app.use(routes);
+
+
+
+
+
+app.listen(3333);
+console.log("iniciada em localhost:3333");
